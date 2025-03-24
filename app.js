0dd42b65148f8c378877f87404a7abccef3466f4
Ilya Bielov <ibioriginalibi@gmail.com>
2025-03-24 17:00:16 +0200

    add MongoDB connection setup and update environment variables

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from './config/env.js';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import subscriptionRoutes from './routes/subscription.routes.js';
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -16,8 +17,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on port http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
