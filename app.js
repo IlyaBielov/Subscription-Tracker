8382878bec74030b2bd8c9d4afa08de6740d4dcf
Ilya Bielov <ibioriginalibi@gmail.com>
2025-03-24 17:50:45 +0200

    add error handling middleware for improved error responses

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from "cookie-parser";
 
 import { PORT } from './config/env.js';
 
@@ -6,12 +7,19 @@ import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import subscriptionRoutes from './routes/subscription.routes.js';
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscriptions', subscriptionRoutes);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API!');
